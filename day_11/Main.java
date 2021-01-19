c313d05cd27714fd94f5732e01bbcf403edb16dd
Rocky <rockygitt@gmail.com>
2021-01-19 01:16:56 +0100

    AoC Problem 11

--- day_11/Main.java
+++ day_11/Main.java
@@ -1,58 +1,39 @@
 package day_11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+  public static final int DIM = 300;
+
   static int gridSerialNumber = 2866;
+  static List<List<Integer>> powers = new ArrayList<>();
 
   public static void main(String[] args) {
-    int[] grid = getMaxPowerPosition();
+    Main.setPowers();
 
     System.out.print("Top-right corner of 3x3 grid with max power (1): ");
-    System.out.println(getPositionString(grid[0], grid[1]));
-
-    int maxPower = 0;
-    int dimension = 0;
-
-    for (int i = 1; i <= 300; i++) {
-      for (int j = 1; j <= 300; j++) {
-        for (int d = 0; d < getMaxGridDimension(i, j, 300); d++) {
-          int power = getGridPower(i, j, d);
-
-          if (power > maxPower) {
-            maxPower = power;
-            grid[0] = i;
-            grid[1] = j;
-            dimension = d;
-          }
-        }
-      }
-    }
+    System.out.println(getMaxPowerPosition(3));
 
     System.out.print("Top-right corner identifier of grid with max power (2): ");
-    System.out.println(getPositionString(grid[0], grid[1], dimension));
+    System.out.println(getMaxPowerPosition());
   }
 
-  private static int[] getMaxPowerPosition() {
-    int maxPower = 0;
-    int[] grid = new int[2];
+  private static int getPower(int x, int y) {
+    return ((((x + 10) * y + Main.gridSerialNumber) * (x + 10)) / 100) % 10 - 5;
+  }
 
-    for (int i = 1; i <= 300 - 2; i++) {
-      for (int j = 1; j <= 300 - 2; j++) {
-        int power = getGridPower(i, j, 3);
+  private static void setPowers() {
+    for (int y = 1; y <= Main.DIM; y++) {
+      List<Integer> row = new ArrayList<>();
 
-        if (power > maxPower) {
-          maxPower = power;
-          grid[0] = i;
-          grid[1] = j;
-        }
+      for (int x = 1; x <= Main.DIM; x++) {
+        row.add(getPower(x, y));
       }
+
+      powers.add(row);
     }
-
-    return grid;
-  }
-
-  private static int getMaxGridDimension(int i, int j, int total) {
-    return i > j ? total - i : total - j;
   }
 
   private static int getGridPower(int i, int j, int d) {
@@ -60,23 +41,77 @@ public class Main {
 
     for (int x = 0; x < d; x++) {
       for (int y = 0; y < d; y++) {
-        power += getPower(i + x, j + y);
+        power += Main.powers.get(j + y - 1).get(i + x - 1);
       }
     }
 
     return power;
   }
 
+  private static String getMaxPowerPosition(int gridSize) {
+    int maxPower = 0;
+    int[] grid = new int[2];
+
+    for (int i = 1; i <= Main.DIM - 2; i++) {
+      for (int j = 1; j <= Main.DIM - 2; j++) {
+        int power = getGridPower(i, j, gridSize);
+
+        if (power > maxPower) {
+          maxPower = power;
+          grid = new int[] {i, j};
+        }
+      }
+    }
+
+    return getPositionString(grid[0], grid[1]);
+  }
+
+  private static int getGridPower(int i, int j, int d, int lastPowerGrid) {
+    int power = lastPowerGrid;
+
+    for (int x = 0; x < d; x++) {
+      power += Main.powers.get(j + d - 2).get(i + x - 1);
+    }
+
+    for (int y = 0; y < d - 1; y++) {
+      power += Main.powers.get(j + y - 1).get(i + d - 2);
+    }
+
+    return power;
+  }
+
+  private static String getMaxPowerPosition() {
+    int[] grid = new int[3];
+    int maxPower = 0;
+
+    for (int i = 1; i <= Main.DIM; i++) {
+      for (int j = 1; j <= Main.DIM; j++) {
+        int power = 0;
+
+        for (int d = 1; d <= getMaxGridDimension(i, j); d++) {
+          power = getGridPower(i, j, d, power);
+
+          if (power > maxPower) {
+            maxPower = power;
+            grid = new int[] {i, j, d};
+          }
+        }
+      }
+    }
+
+    return getPositionString(grid[0], grid[1], grid[2]);
+  }
+
+  private static int getMaxGridDimension(int i, int j) {
+    return i > j ? Main.DIM - i : Main.DIM - j;
+  }
+
   private static String getPositionString(int x, int y) {
-    return new StringBuilder().append(x).append(",").append(y).toString();
+    return x + "," + y;
   }
 
   private static String getPositionString(int x, int y, int d) {
-    return new StringBuilder().append(x).append(",").append(y).append(",").append(d).toString();
-  }
-
-  private static int getPower(int x, int y) {
-    return ((((x + 10) * y + Main.gridSerialNumber) * (x + 10)) / 100) % 10 - 5;
+    return Main.getPositionString(x, y) + "," + d;
   }
 
 }
